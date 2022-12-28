2a1e0306f9e27d082537f46df47ed82c96419912
RomanZlobin <rvzlobin@gmail.com>
2022-12-28 17:32:09 +0700

    allure added

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -2,6 +2,9 @@ package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,8 +19,17 @@ class DeliveryTest {
 
     @BeforeEach
     void setup() {
-       Configuration.holdBrowserOpen = true;
-       open("http://localhost:9999");
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+    }
+
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
 
